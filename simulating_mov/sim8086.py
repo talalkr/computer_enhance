4ccf2d8e946773918d6afe0085a49dbfef3fea31
talal_alrashid <talal_alrashid@gepam.com>
2026-03-16 22:20:22 +0300

    simulating ADD, SUB, and CMP

--- simulating_mov/sim8086.py
+++ simulating_mov/sim8086.py
@@ -1,31 +1,63 @@
 # Simulating what the MOV instruction does
 
 import argparse
+from collections import defaultdict
 from instruction_decoding.sim8086 import decode_to_asm
 
-register_store = {
-    "ax": 0,
-    "bx": 0,
-    "cx": 0,
-    "dx": 0,
-    "sp": 0,
-    "bp": 0,
-    "si": 0,
-    "di": 0,
-}
+MOV = "mov"
+ADD = "add"
+SUB = "sub"
+CMP = "cmp"
 
-def debug_mov(instructions: list[str]) -> list[str]:
+register_store = defaultdict(int)
+
+def is_immediate(s) -> bool:
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+def debug_asm(instructions: list[str]) -> list[str]:
     for idx in range(len(instructions)):
-        data = instructions[idx][4:]  # remove 'mov '
-        reg_dest, src = data.split(", ")
-        src_val = int(src) if src.isdigit() else register_store[src]
+        opcode = instructions[idx][:3]
+        reg_dest, src = instructions[idx][4:].split(", ")
 
-        instructions[idx] = f"{instructions[idx]} ; {reg_dest}:{hex(register_store[reg_dest])}->{hex(src_val)}"
-        register_store[reg_dest] = src_val
+        src_val = (int(src) if is_immediate(src) else register_store[src]) & 0xFFFF
 
-    # Print final registers
+        if is_immediate(src):
+            instructions[idx] = instructions[idx].replace(src, str(src_val))
+
+        old_val = register_store[reg_dest]
+
+        if opcode == MOV:
+            register_store[reg_dest] = src_val
+        elif opcode == ADD:
+            register_store[reg_dest] = (old_val + src_val) & 0xFFFF
+        elif opcode == SUB:
+            register_store[reg_dest] = (old_val - src_val) & 0xFFFF
+
+        if opcode in (MOV, ADD, SUB):
+            instructions[idx] += f" ; {reg_dest}:{hex(old_val)}->{hex(register_store[reg_dest])}"
+
+        # Handle flags
+        if opcode in (ADD, SUB):
+            new_val = register_store[reg_dest]
+            if new_val == 0:
+                instructions[idx] += " flags:->Z"
+            elif new_val >= 0x8000:
+                instructions[idx] += " flags:->S"
+
+        if opcode == CMP:
+            cmp_val = (old_val - src_val) & 0xFFFF
+            new_inst = f"{instructions[idx]} ; flags:"
+            instructions[idx] = f"{new_inst}->Z" if cmp_val == 0 else \
+                                 f"{new_inst}->S" if cmp_val >= 0x8000 else \
+                                 f"{new_inst}S->"
+
     instructions.append("\nFinal registers:")
-    instructions.extend([f"\t{k}: {hex(v)} ({v})" for k, v in register_store.items()])
+    instructions.extend([f"\t{k}: {hex(v)} ({v})" for k, v in register_store.items() if v != 0])
+
     return instructions
 
 
@@ -45,7 +77,7 @@ def main(filename: str, debug: bool = False) -> None:
         
     asm_inst = decode_to_asm(asm_bytes)
     if debug:
-        asm_inst = debug_mov(asm_inst)
+        asm_inst = debug_asm(asm_inst)
     write_to_file(f"{filename}_py.asm", asm_inst)
 
 if __name__ == "__main__":
